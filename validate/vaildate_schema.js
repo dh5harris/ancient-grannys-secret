1a98aff50bc19f99fe019d8d776bd3c1981f6565
dh5harris <dh5harris@hotmail.com>
2023-03-25 21:14:04 -0600

    added user and recipe

--- validate/vaildate_schema.js
+++ validate/vaildate_schema.js
@@ -8,4 +8,20 @@ const cuisineSchema = Joi.object({
 	cuisineName: Joi.string().alphanum().required()
 })
 
-module.exports = {mealSchema, cuisineSchema}
+const recipeSchema = Joi.object({
+	recipeName: Joi.string().alphanum().required(),
+	ingredients: Joi.array().items(
+		Joi.string().alphanum().required()
+	).required(),
+	directions: Joi.string().required()
+})
+
+const userSchema = Joi.object({
+	userName: Joi.string().alphanum().required(),
+	password: Joi.string().pattern(/^[a-zA-Z0-9]+$/).required(),
+	firstName: Joi.string().alphanum().required(),
+	lastName: Joi.string().alphanum().required(),
+	email: Joi.string().email().lowercase().required()
+})
+
+module.exports = {mealSchema, cuisineSchema, recipeSchema, userSchema}
