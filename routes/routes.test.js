5540c58b7dbbb76eb2ed455f489a478d8ac135fe
AlexxCourter <alex.courter@gmail.com>
2023-04-04 14:43:02 -0400

    Routes folder Jest testing

---
+++ routes/routes.test.js
@@ -0,0 +1,47 @@
+const request = require("supertest");
+const express = require('express');
+const bodyParser = require('body-parser')
+// const {MongoClient} = require('mongodb');
+const app = express();
+
+// const port = process.env.PORT || 8080;
+// //create a mock app here so that tests can run on routes properly.
+// //Why not server.js?: gets stuck without auth secret, which we may not want to reveal to these tests
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+  	res.setHeader('Access-Control-Allow-Origin', '*');
+ 		next();
+  })
+	.use('/', require('./tests/index'));
+
+//employs mock routes for each of the collections to test functionality and connection.
+//mock routes will return mock data as expected in production set up.
+describe("Test routes", () => {
+
+  test("GET /user/ Response should be 200 with user list", () => {
+    return request(app)
+      .get("/test/user")
+      .expect(200);
+  });
+  test("GET /user/:id Response should be 200 with single user", () => {
+    return request(app)
+      .get("/test/user/1")
+      .expect(200);
+  });
+  test("POST /user/ Response should be 201 with acknowledgement of response", () => {
+    return request(app)
+      .post("/test/user")
+      .expect(201);
+  });
+  test("PUT /user/:id Response should be 204", () => {
+    return request(app)
+      .post("/test/user")
+      .expect(201);
+  });
+  test("DELETE /user/:id Response should be 200", () => {
+    return request(app)
+      .post("/test/user")
+      .expect(201);
+  });
+});
---
+++ routes/tests/index.js
@@ -0,0 +1,132 @@
+//Jest testing mock functions for routes folder
+
+const express = require('express');
+const router = express.Router();
+
+//mock user list
+const mockUserList = [
+    {
+      _id: "640be1331293bc944376825c",
+      userName: "testUser1",
+      firstNam: "Stanley",
+      lastName: "Jablonski",
+      email: "stan@jablonski.com",
+      password: "ARW2388@2",
+      bio: "Short description of user",
+      image: "image link or other data",
+      recipes: [
+        {
+          "id": "63f1be5d8d3384a384ba324b"
+        }
+      ]
+    },
+    {
+        _id: "640be1331293bc944376825d",
+        userName: "testUser2",
+        firstNam: "Stanley",
+        lastName: "Jablonski",
+        email: "stan@jablonski.com",
+        password: "ARW2388@2",
+        bio: "Short description of user",
+        image: "image link or other data",
+        recipes: [
+            {
+                "id": "63f1be5d8d3384a384ba324b"
+            }
+            ]
+      }
+  ]
+
+//User
+//mocks all routes for the User collection.
+//does not test Database functions. That is available in controllers.test.js.
+router.get('/test/user', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(mockUserList);});
+router.get('/test/user/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(200).json(mockUserList[0]);
+});
+router.post('/test/user/', (req,res)=>{
+    const mockUser = mockUserList[0];
+    res.setHeader('Content-Type', 'application/json');
+	res.status(201).json(mockUser);
+});
+router.put('/test/user/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(204).send();
+});
+router.delete('/test/user/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(200).send();
+});
+
+const mockRecipeList = [
+    {
+        recipeName: "Chocolate Milk",
+        ingredients: [
+          "Chocolate syrup"
+        ],
+        directions: "Mix chocolate syrup into milk.",
+        isPrivate: true
+      }
+];
+//RECIPE COLLECTION
+router.get('/test/recipe', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(mockRecipeList);});
+router.get('/test/recipe/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(200).json(mockRecipeList[0]);
+});
+router.post('/test/recipe/', (req,res)=>{
+    const mockRecipe = mockRecipeList[0];
+    res.setHeader('Content-Type', 'application/json');
+	res.status(201).json(mockRecipe);
+});
+router.put('/test/recipe/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(204).send();
+});
+router.delete('/test/recipe/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(200).send();
+});
+
+const mockmealList = [
+    {
+        _id: "640be3721293bc944376825e",
+        mealName: "Breakfast"
+      }
+]
+//MEALS COLLECTION
+router.get('/test/meal', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(mockmealList);});
+router.get('/test/meal/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(200).json(mockmealList[0]);
+});
+router.post('/test/meal/', (req,res)=>{
+    const mockmeal = mockmealList[0];
+    res.setHeader('Content-Type', 'application/json');
+	res.status(201).json(mockmeal);
+});
+router.put('/test/meal/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(204).send();
+});
+router.delete('/test/meal/:id', (req,res)=>{
+    res.setHeader('Content-Type', 'application/json');
+	res.status(200).send();
+});
+
+
+const mockCuisineList = [
+    {
+        _id: "640be3721293bc944376825e",
+        cuisineName: "Italian"
+      }
+]
+
+module.exports = router;
